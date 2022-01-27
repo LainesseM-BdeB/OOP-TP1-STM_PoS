3ea6c3397ba51d22c0edcf198768226167daacee
LaZoRBear <lainesse.maxime@hotmail.com>
2022-01-27 08:55:31 -0500

    Initial Commit

---
+++ src/qc/ca/bdeb/_420_a02_bb/tp1/Main.java
@@ -0,0 +1,8 @@
+package qc.ca.bdeb._420_a02_bb.tp1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/qc/ca/bdeb/_420_a02_bb/tp1/menu.java
@@ -0,0 +1,7 @@
+package qc.ca.bdeb._420_a02_bb.tp1;
+
+public class menu {
+    private void menu(String name, String title, String desc, String[] options) {
+
+    }
+}
